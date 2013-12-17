b15aff17e13aead78c8bcbaa98346316e719c0e1
Ody Mbegbu <odytrice@gmail.com>
2013-12-17 20:54:00 +0100

    Initial Commit

---
+++ MoveShape/MoveShapeHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movr.MoveShape
+{
+    /// <summary>
+    /// A Hub is a thing that clients connect to to talk to each other
+    /// The server can also use the hub to broadcast out to connected clients
+    /// </summary>
+    [HubName("MoveShape")]
+    public class MoveShapeHub : Hub
+    {
+        //Server Side Method to Move the Shape of the Global State
+        public void MoveShape(int x, int y)
+        {
+            ///Call Shaped Moved on Client Code
+            ///Passing the current connectionID and the co-ordinates
+            Clients.All.shapeMoved(Context.ConnectionId, x, y);
+        }
+    }
+}
---
+++ moveshape.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="Scripts/jquery-1.6.4.js" />
+/// <reference path="Scripts/jquery.signalR-2.0.1.js" />
+/// <reference path="Scripts/jquery-ui-1.10.3.js" />
+
+$(function () {
+    //Create Representation of the Server's Move Shape
+    var hub = $.connection.MoveShape,
+        $shape = $("#shape");
+
+    //Extend the Hub and add shapeMoved that is to be called from the server
+    $.extend(hub.client, {
+        shapeMoved: function (cid, x, y) {
+            //Check to make sure that the invocation did not come from myself
+            if (cid !== $.connection.hub.id) {
+                //Update the position of the object on the screen
+                $shape.css({ left: x, top: y });
+            }
+        }
+    });
+
+
+    ///I have to start the connection with the server and then wire up the client behaviour
+    ///hub.start() is asynchronous
+    $.connection.hub.start().done(function () {
+        //Make the shape draggable
+        $shape.draggable({
+            //When the drag takes place, call the method moveshape on the Hub
+            drag: function () {
+                hub.server.moveShape(this.offsetLeft, this.offsetTop || 0)
+            }
+        })
+    });
+});
